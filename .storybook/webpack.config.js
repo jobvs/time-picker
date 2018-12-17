5014c748187cec9ef7b763aa8c4240e524efb557
Job van Stiphout <job.van.stiphout@mendix.com>
2018-12-17 15:00:59 +0100

    Test reports for CI

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
 module.exports = (baseConfig, env, config) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve("ts-loader")
-  });
+    config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        loader: require.resolve("ts-loader")
+    });
 
-  config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.extensions.push(".ts", ".tsx");
 
-  return config;
+    config.plugins = config.plugins.filter(({ constructor }) => constructor.name !== "ProgressPlugin");
+
+    return config;
 };
